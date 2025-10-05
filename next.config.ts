b9bf83a4640b28f2f0b8139b01a1379fd1b14e85
wokrelorian-hub <wokrelorian@gmail.com>
2025-10-05 14:14:09 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // Add this line
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
